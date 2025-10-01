d9e40189e4dec2a1d70ae3ba50585c524af56afd
Seangly <seanglychou62@gmail.com>
2025-10-01 22:56:44 +0700

    fix page

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${poppins.variable} antialiased bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-200 transition-colors duration-300`}
-      >
+      <body className={`${poppins.variable} antialiased`}>
         <SessionWrapper>
           <ThemeProvider 
            attribute="class"
